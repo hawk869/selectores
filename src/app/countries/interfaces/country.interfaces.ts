94ae78ad29da7f4cd36d537ef7a5a0af0f3b857d
Daniel Esteban <decepeda@misena.edu.co>
2023-10-02 16:30:29 -0500

    primer selector, se crea interface con quickType

---
+++ src/app/countries/interfaces/country.interfaces.ts
@@ -0,0 +1,161 @@
+export enum Region {
+    Africa   = 'Africa',
+    Americas = 'Americas',
+    Oceania  = 'Oceania',
+    Asia     = 'Asia',
+    Europe   = 'Europe',
+}
+
+export interface Country {
+    name:         Name;
+    tld?:         string[];
+    cca2:         string;
+    ccn3?:        string;
+    cca3:         string;
+    cioc?:        string;
+    independent?: boolean;
+    status:       Status;
+    unMember:     boolean;
+    currencies:   Currencies;
+    idd:          Idd;
+    capital:      string[];
+    altSpellings: string[];
+    region:       string;
+    subregion:    Subregion;
+    languages:    { [key: string]: string };
+    translations: { [key: string]: Translation };
+    latlng:       number[];
+    landlocked:   boolean;
+    borders?:     string[];
+    area:         number;
+    demonyms:     Demonyms;
+    flag:         string;
+    maps:         Maps;
+    population:   number;
+    gini?:        { [key: string]: number };
+    fifa?:        string;
+    car:          Car;
+    timezones:    string[];
+    continents:   string[];
+    flags:        Flags;
+    coatOfArms:   CoatOfArms;
+    startOfWeek:  StartOfWeek;
+    capitalInfo:  CapitalInfo;
+    postalCode?:  PostalCode;
+}
+
+export interface CapitalInfo {
+    latlng: number[];
+}
+
+export interface Car {
+    signs: string[];
+    side:  Side;
+}
+
+export enum Side {
+    Left = "left",
+    Right = "right",
+}
+
+export interface CoatOfArms {
+    png?: string;
+    svg?: string;
+}
+
+export interface Currencies {
+    MDL?: TartuGecko;
+    RSD?: TartuGecko;
+    DKK?: TartuGecko;
+    EUR?: TartuGecko;
+    RON?: TartuGecko;
+    GIP?: TartuGecko;
+    NOK?: TartuGecko;
+    GBP?: TartuGecko;
+    RUB?: TartuGecko;
+    SEK?: TartuGecko;
+    MKD?: TartuGecko;
+    ALL?: TartuGecko;
+    BAM?: BAM;
+    UAH?: TartuGecko;
+    GGP?: TartuGecko;
+    CHF?: TartuGecko;
+    HUF?: TartuGecko;
+    IMP?: TartuGecko;
+    PLN?: TartuGecko;
+    FOK?: TartuGecko;
+    JEP?: TartuGecko;
+    CZK?: TartuGecko;
+    ISK?: TartuGecko;
+    BYN?: TartuGecko;
+    BGN?: TartuGecko;
+}
+
+export interface TartuGecko {
+    name:   string;
+    symbol: string;
+}
+
+export interface BAM {
+    name: string;
+}
+
+export interface Demonyms {
+    eng:  Eng;
+    fra?: Eng;
+}
+
+export interface Eng {
+    f: string;
+    m: string;
+}
+
+export interface Flags {
+    png:  string;
+    svg:  string;
+    alt?: string;
+}
+
+export interface Idd {
+    root:     string;
+    suffixes: string[];
+}
+
+export interface Maps {
+    googleMaps:     string;
+    openStreetMaps: string;
+}
+
+export interface Name {
+    common:     string;
+    official:   string;
+    nativeName: { [key: string]: Translation };
+}
+
+export interface Translation {
+    official: string;
+    common:   string;
+}
+
+export interface PostalCode {
+    format: string;
+    regex:  string;
+}
+
+export enum StartOfWeek {
+    Monday = "monday",
+}
+
+export enum Status {
+    OfficiallyAssigned = "officially-assigned",
+    UserAssigned = "user-assigned",
+}
+
+export enum Subregion {
+    CentralEurope = "Central Europe",
+    EasternEurope = "Eastern Europe",
+    NorthernEurope = "Northern Europe",
+    SoutheastEurope = "Southeast Europe",
+    SouthernEurope = "Southern Europe",
+    WesternEurope = "Western Europe",
+}
